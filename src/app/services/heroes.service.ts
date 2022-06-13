002bf8f6d36af73d6507ecd000b6e4eda9529454
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-13 10:58:20 -0500

    busqueda en barra.

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -67,6 +67,21 @@ export class HeroesService {
     return this.heroes[<any>i]
   }
 
+  buscarHeroes(termino: string) {
+    let heroesArr: Heroe[] = []
+
+    termino = termino.toLowerCase()
+
+    for (let heroe of this.heroes) {
+      let nombre = heroe.nombre.toLowerCase()
+
+      if (nombre.indexOf(termino) >= 0) {
+        heroesArr.push(heroe)
+      }
+    }
+    return heroesArr
+  }
+
 }
 
 export interface Heroe {
